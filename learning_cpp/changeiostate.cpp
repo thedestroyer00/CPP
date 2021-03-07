7fbd5bef148f5a97705c63a1615191f60883a017
BikramNischal <53038045+PsychicGeek@users.noreply.github.com>
2021-03-07 13:36:22 +0545

    Create changeiostate.cpp

---
+++ learning_cpp/changeiostate.cpp
@@ -0,0 +1,45 @@
+//Manually setting iostate in cpp
+
+# include <iostream>
+
+std::istream& readin(std::istream& s){
+    int val;
+    std::cout << "enter number : ";
+    s >> val;
+    s.setstate(std::ios::failbit); //set istream to failstate
+    
+    if(val != 0){
+        s.clear(s.goodbit); //set istream to goodstate
+        std::cout << val << std::endl;
+        return s;
+    }
+    else{
+        std::cout << "Failbit set!" << std::endl;
+        return s;
+    } 
+        
+}
+
+int main(){
+   
+    while(true){
+        std::istream& in = readin(std::cin);
+        if(in)
+            std::cout << "Goodbit Return!" << std::endl;
+        else{
+            std::cout << "Reseting to goodbit!" << std::endl;
+            in.clear(in.goodbit); //set istream to goodstate
+            if(in){
+                std::cout << "Goodbit set!" << std::endl;
+                break; 
+            }
+            else{
+                std::cout << "Fail to set!" << std::endl;
+                break;
+            }
+        }
+    }
+
+
+    return 0;
+}
